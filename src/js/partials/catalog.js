3d42e6d71d02be5b6cd59d7b9e103eb8a86c55e3
db21 <you@example.com>
2019-07-09 15:58:07 +0300

    Таблица готова. Осталась стилизация скролла. Начинаю недолюбливать таблицы в html. Слишком муторно

--- src/js/partials/catalog.js
+++ src/js/partials/catalog.js
@@ -1,6 +1,6 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-    $('.left_side-catalog_tabs .dt ul a').click(function() {
+    $('.left_side-catalog_tabs .dt ul a').click(function () {
         $(this).closest('.dt').find('a').removeClass('active');
         $(this).addClass('active');
         $(this).closest('.left_side-catalog_tabs').find('.item').removeClass('active')
@@ -16,8 +16,26 @@ $(document).ready(function() {
     $('.catalog-page .content-catalog .tab-title').click(function () {
         $(this).addClass('active').siblings('.tab-title').removeClass('active');
         var tab = $(this).data('tab');
-        $('#tab-'+tab).addClass('active').siblings('.tab').removeClass('active');
+        $('#tab-' + tab).addClass('active').siblings('.tab').removeClass('active');
     });
 
 
+
+
+
+    $('#customScroll').attr("max", $('#scroll table tbody').width() - $('#scroll').width());
+
+    $('#scroll').scroll(function () {
+        console.log($('#scroll').scrollLeft() + "    width=" + ($('#scroll table tbody').width() - $('#scroll').width()));
+        $('#customScroll').val($('#scroll').scrollLeft());
+    });
+
+    $('#customScroll').on('mousemove change', function () {
+        console.log($('#customScroll').val());
+        $('#scroll').scrollLeft($('#customScroll').val());
+    });
+    $(window).resize(function () {
+        $('#customScroll').attr("max", $('#scroll table tbody').width() - $('#scroll').width());
+
+    });
 });
