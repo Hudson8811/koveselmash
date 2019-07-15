73461b36df115b266069509bd9b0c9b360b9f820
PipZip <37911683+PipZip@users.noreply.github.com>
2019-07-15 14:34:59 +0600

    temp

--- build/js/main.js
+++ build/js/main.js
@@ -892,30 +892,11 @@ $(document).ready(function () {
         }
     });
 
+});
 $(document).ready(function() {
-    $('.home-page .big-tabs .tab-title').click(function () {
+    $('.contacts-page .tabs .tab-title').click(function () {
         $(this).addClass('active').siblings('.tab-title').removeClass('active');
-        var tabb = $(this).data('tabb');
-        $('#tabb-'+tabb).addClass('active').siblings('.tab').removeClass('active');
-        if (tabb == 3){
-            var reviewSwiper = new Swiper ('.big-tabs #tabb-3 .swiper-container', {
-                slidesPerView: 2,
-                spaceBetween: 30,
-                pagination: {
-                    el: '.big-tabs #tabb-3 .swiper-pagination',
-                    clickable: true,
-                },
-
-                navigation: {
-                    nextEl: '.big-tabs #tabb-3 .swiper-button-next',
-                    prevEl: '.big-tabs #tabb-3 .swiper-button-prev',
-                },
-                breakpoints: {
-                    1000: {
-                        slidesPerView: 1,
-                    },
-                }
-            });
-        }
+        var tab = $(this).data('tab');
+        $('#tab-'+tab).addClass('active').siblings('.tab').removeClass('active');
     });
 });
