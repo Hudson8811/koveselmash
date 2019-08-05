372e7a85eb2ff06da2a39262c89b44c65694b4df
db21 <you@example.com>
2019-08-05 15:06:22 +0300

    repres script

--- src/js/partials/repres.js
+++ src/js/partials/repres.js
@@ -10,14 +10,9 @@ $(document).ready(function () {
             function (e) {
                 $('#hb_' + $(this).attr('data-country')).addClass('visible');
 
-                /*   var parentOffset = $(this).parent().parent().offset();
-                   var relX = e.pageX - parentOffset.left;
-                   var relY = e.pageY - parentOffset.top;
-                   $('#hb_' + $(this).attr('data-country')).css({top: relY, right: 'calc(100% - '+relX+'px - 10px)'});*/
-                //  $('#hb_' + $(this).attr('data-country')).css({top: relY, left: relX});
                 if ($(window).width() <= 700) {
                     $('.country').mousemove(function (e) {
-                        var parentOffset = $(this).parent().parent().offset();
+                        var parentOffset = $(this)/*.parent()*/.parent().offset();
                         var relX = e.pageX - parentOffset.left;
                         var relY = e.pageY - parentOffset.top;
                         $('#hb_' + $(this).attr('data-country')).css({ top: relY - 80, right: 'calc(100% - ' + relX + 'px - 35px' });
@@ -25,7 +20,7 @@ $(document).ready(function () {
                 }
                 else {
                     $('.country').mousemove(function (e) {
-                        var parentOffset = $(this).parent().parent().offset();
+                        var parentOffset = $(this)/*.parent()*/.parent().offset();
                         var relX = e.pageX - parentOffset.left;
                         var relY = e.pageY - parentOffset.top;
                         $('#hb_' + $(this).attr('data-country')).css({ top: relY - 80, right: 'calc(100% - ' + relX + 'px - 35px' });
@@ -41,9 +36,58 @@ $(document).ready(function () {
 
         );
 
+        $('.map path').click(function (e) {
+            var $id=$('#content_'+$(this).attr('data-country'));
+            $id.click();
+             $('.toggle .toggle_item:last-child').click();
+           // $id.click();
+        });
 
 
 
+
+        $('.toggle .toggle_item').click(
+            function (e) {
+                //console.log($(this).index());
+                var $this = $(this);
+                if ($this.index() == 0) {
+                    $('#page-wrapper > div.map').fadeIn(200);
+                    $('#page-wrapper > div.map-text-container.container > div > div.map-text').fadeIn(200);
+                    $('#page-wrapper').removeClass('repres-page-list');
+                    // $('#page-wrapper > div.map-text-container.container > .left').fadeOut(200);
+
+                }
+
+                if ($this.index() == 1) {
+                    $('#page-wrapper > div.map').fadeOut(200);
+                    $('#page-wrapper > div.map-text-container.container > div > div.map-text').fadeOut(200);
+                    $('#page-wrapper').addClass('repres-page-list');
+                    $('#page-wrapper > .diller-tabs').fadeIn(200);
+                }
+
+                $this.addClass('active').siblings().removeClass('active');
+            }
+        );
+        $('.category .item .contacts').slideUp(0);
+        $('.category .item .name').click(
+            function (e) {
+                $(this).siblings('.contacts').stop().slideToggle(200, function () {
+
+                    if ($(this).is(':visible')) {
+                        $(this).css('display', 'flex');
+                    }
+
+                });
+                $(this).parent().toggleClass('closed');
+
+
+            }
+        );
+
+        $('#page-wrapper > div.map-text-container.container > div > div.diller-tabs > div.tabs > div.item').click(function (e) {
+            $(this).addClass('active').siblings().removeClass('active');
+            $("#page-wrapper > div.map-text-container.container > div > div.diller-tabs > div.content").eq($(this).index()).addClass('active').siblings('.content').removeClass('active');
+        });
 
 
         function makeAdaptive__repres_page() {
